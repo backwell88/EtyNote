34203c81a261122e9e191f98317ec468d0db5782
backwell88 <jdu003623@gmail.com>
2026-02-12 12:36:19 +0800

    feat: add word entry model

---
+++ Core/Models/WordEntry.swift
@@ -0,0 +1,13 @@
+﻿import Foundation
+
+struct WordEntry: Codable {
+    let word: String
+    let partOfSpeech: [String]
+    let englishMeaning: [String]
+    let chineseMeaning: [String]
+    let root: Morphology
+    let prefix: Morphology
+    let suffix: Morphology
+    let relatedWords: [String]
+    let variants: [String]
+}
