e4ad07bc40b61dbc087d4ba14276c46dd4d4c1aa
backwell88 <jdu003623@gmail.com>
2026-02-12 14:35:05 +0800

    feat: add search service for title query

---
+++ Core/Services/SearchService.swift
@@ -0,0 +1,11 @@
+﻿import Foundation
+
+enum SearchService {
+    static func searchTitles(
+        keyword: String,
+        in documentsURL: URL
+    ) throws -> [String] {
+        let titles = try MarkdownStoreService.loadTitles(in: documentsURL)
+        return IndexService.search(keyword, in: titles)
+    }
+}
