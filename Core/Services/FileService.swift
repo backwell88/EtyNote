676866d6b81d6971fc4bccf1f9426d14d8a3edf7
backwell88 <jdu003623@gmail.com>
2026-02-12 12:45:03 +0800

    feat: add local markdown append service

---
+++ Core/Services/FileService.swift
@@ -0,0 +1,28 @@
+﻿import Foundation
+
+enum FileService {
+    static let folderName = "EtyNote"
+    static let fileName = "etynote.md"
+
+    static func append(_ markdown: String, in documentsURL: URL) throws {
+        let fm = FileManager.default
+        let folderURL = documentsURL.appendingPathComponent(folderName, isDirectory: true)
+        let fileURL = folderURL.appendingPathComponent(fileName)
+
+        if !fm.fileExists(atPath: folderURL.path) {
+            try fm.createDirectory(at: folderURL, withIntermediateDirectories: true)
+        }
+
+        let data = Data(markdown.utf8)
+
+        if !fm.fileExists(atPath: fileURL.path) {
+            try data.write(to: fileURL)
+            return
+        }
+
+        let handle = try FileHandle(forWritingTo: fileURL)
+        defer { try? handle.close() }
+        try handle.seekToEnd()
+        try handle.write(contentsOf: data)
+    }
+}
