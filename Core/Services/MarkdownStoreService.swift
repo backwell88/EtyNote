166859e9f2fcc7585c609085d861d871292980cc
backwell88 <jdu003623@gmail.com>
2026-02-12 14:34:30 +0800

    feat: add markdown read and title load service

---
+++ Core/Services/MarkdownStoreService.swift
@@ -0,0 +1,22 @@
+﻿import Foundation
+
+enum MarkdownStoreService {
+    static func fileURL(in documentsURL: URL) -> URL {
+        documentsURL
+            .appendingPathComponent(FileService.folderName, isDirectory: true)
+            .appendingPathComponent(FileService.fileName)
+    }
+
+    static func loadMarkdown(in documentsURL: URL) throws -> String {
+        let url = fileURL(in: documentsURL)
+        guard FileManager.default.fileExists(atPath: url.path) else {
+            return ""
+        }
+        return try String(contentsOf: url, encoding: .utf8)
+    }
+
+    static func loadTitles(in documentsURL: URL) throws -> [String] {
+        let markdown = try loadMarkdown(in: documentsURL)
+        return IndexService.extractTitles(from: markdown)
+    }
+}
