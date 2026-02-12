96bebd74590256030d260f4c131b2866e5ee25dd
backwell88 <jdu003623@gmail.com>
2026-02-12 12:45:43 +0800

    feat: add entry save service

---
+++ Core/Services/EntrySaveService.swift
@@ -0,0 +1,9 @@
+﻿import Foundation
+
+enum EntrySaveService {
+    static func save(_ entry: WordEntry, in documentsURL: URL) throws -> Bool {
+        guard let markdown = EntryPipeline.process(entry) else { return false }
+        try FileService.append(markdown, in: documentsURL)
+        return true
+    }
+}
