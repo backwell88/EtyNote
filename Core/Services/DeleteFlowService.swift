6f2881f4d7e0a0d791ab6bd3c3a6ad3c00956d22
backwell88 <jdu003623@gmail.com>
2026-02-12 14:36:47 +0800

    feat: add delete flow service with title reload

---
+++ Core/Services/DeleteFlowService.swift
@@ -0,0 +1,17 @@
+﻿import Foundation
+
+struct DeleteResult {
+    let deleted: Bool
+    let titles: [String]
+}
+
+enum DeleteFlowService {
+    static func deleteAndReloadTitles(
+        word: String,
+        in documentsURL: URL
+    ) throws -> DeleteResult {
+        let deleted = try EntryDeleteService.delete(word: word, in: documentsURL)
+        let titles = try MarkdownStoreService.loadTitles(in: documentsURL)
+        return DeleteResult(deleted: deleted, titles: titles)
+    }
+}
